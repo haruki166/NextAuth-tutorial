0d5569e8016a7e44f8effc5febd26c4f1aa9f40a
haruki suziuki <harukiti166@gmail.com>
2023-07-10 18:58:00 +0900

    set up app

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+import { type } from "os";
+import React from "react";
+
+type LayoutProps = Required<{
+  readonly children: React.ReactNode;
+}>;
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center text-3xl">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
