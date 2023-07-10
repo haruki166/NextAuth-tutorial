37b0fffdba49aaa1a54ebcc2e2366b8cab35e1a0
haruki suziuki <harukiti166@gmail.com>
2023-07-11 00:00:36 +0900

    認証ガードコンポーネント追加

---
+++ components/AuthGuard.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import React, { useEffect } from "react";
+
+//ユーザーが認証済みかどうかを判断
+//認証ガード（AuthGuard）として機能し、子要素を表示する前にユーザーの認証ステータスをチェック
+const AuthGuard = ({ children }: { children: React.ReactNode }): any => {
+  // status には、authenticated・unauthenticated・loading のいずれかが格納されます
+  const { status } = useSession(); //認証セッションのステータスを取得
+  const router = useRouter();  Next.jsのルーターオブジェクトを取得;
+
+  //router（=ページ）もしくは status（=認証情報）に変更があった場合useEffect発火
+  useEffect(() => {
+    //status の中身が「unauthenticated」であるか検証
+    if (status === "unauthenticated" && router.pathname != "/auth/signin")
+      // status が unauthenticated の場合でも、現在のパス名が「/auth/signin」だった場合、リダイレクトは行いません
+      router.push("/auth/signin");
+  }, [router, status]);
+  if (status === "loading") return <p>Loading...</p>;
+  if (status === "authenticated") return children;
+};
+
+export default AuthGuard;
+
+//ページのURLやパラメータの情報が格納されている router というオブジェクト
+//status という変数には、認証情報の取得状況・認証済みか否かという情報が格納
+//loading（認証情報の取得中）・unauthenticated（非認証）・authenticated（認証済み）のいずれか
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,38 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { NextComponentType } from "next";
+import { Session } from "next-auth";
 
-export default function App({ Component, pageProps }: AppProps) {
+import { SessionProvider } from "next-auth/react";
+import AuthGuard from "../components/AuthGuard";
+
+//Next.js が提供する AppProps という型を拡張する
+////ページコンポーネントにsessionプロパティとrequireAuthプロパティを追加////
+export type CustomAppProps = AppProps<{ session: Session }> & {
+  Component: NextComponentType & { requireAuth?: boolean };
+};
+//pageProps に session プロパティを追加
+//Component に requireAuth プロパティを追加
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: CustomAppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    //SessionProviderにsessionを渡して全ページ・コンポーネントでセッション情報を取得できるようにした。
+    <SessionProvider session={session}>
+      <Layout>
+        {/* Component.requireAuth の値によって条件分岐を行う。 */}
+        {Component.requireAuth ? (
+          //AuthGuard コンポーネントで囲むことで、認証情報の検証が行われる。
+          <AuthGuard>
+            <Component {...pageProps} />
+          </AuthGuard>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </Layout>
+    </SessionProvider>
   );
 }
